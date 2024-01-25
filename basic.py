884723604dc8558b3420034a55000c92d6242002
Haider9907 <68145735+Haider9907@users.noreply.github.com>
2024-01-25 11:16:03 +0530

    Add files via upload

---
+++ basic.py
@@ -0,0 +1,9 @@
+def sum(x,y):
+    return x+y
+a=int(input("enter no. a"))
+b=int(input("enter no. b"))
+print(sum(a,b))
+
+linkedin.com/in/nitishkashyap-financeguru
+
+https://docs.google.com/spreadsheets/d/135GSkN-9QCjRrGWIJyrJ21jp0rNKaYJ7f2CVMcfmtT0/edit?usp=sharing
